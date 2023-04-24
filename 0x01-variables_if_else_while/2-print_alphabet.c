13b22eee91862fb1651140f2fdceba8dfd590fb9
confiranking <nnekanneka477@gmail.com>
2023-04-24 16:06:41 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints alpha in lower case
+ * Returning; Always 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc * *)
+	{
+		putchar(lc);
+	}
+	putcher('\n');
+	return (0);
+}
